c29c5d6f64c21ca26cead314089a4b79c56d9350
Ryan Wunderly <rwonder@me.com>
2018-07-15 23:42:40 -0700

    Swipable tabs with QR scanner

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -10,5 +10,12 @@ export class AboutPage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  swipe(event) {
+  	if(event.direction === 2) {
+  		this.navCtrl.parent.select(2);
+  	}
+  	if(event.direction === 4) {
+  		this.navCtrl.parent.select(0);
+  	}
+  }
 }
